d2687c202c6661215ebdff39b5aec4e1b7181edb
starrynighttech <starrynight8531@gmail.com>
2026-02-28 23:59:32 +0000

    push to github

---
+++ src/data/App.jsx
@@ -0,0 +1,18 @@
+import { CartProvider } from './context/CartContext';
+import ParticlesBg from './components/ParticlesBg';
+import UltraSmoke from './components/UltraSmoke';
+import './style.css';
+import Shop from './components/Shop';
+import Checkout from './components/Checkout';
+
+export default function App() {
+  return (
+    <CartProvider> 
+      <ParticlesBg/>
+      <div className="Smoke"></div>
+      <div className="hero"> 
+        <img src="/logo.png" width="120"/>
+      
+  
+  );
+</ 
